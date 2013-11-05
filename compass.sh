669ab4ac77acfcbded8be3031b7977ada850967b
alettieri <alettieri@gmail.com>
2013-11-05 09:48:51 -0800

    Added start command, Starts Vagrant and launches site in browser

--- compass.sh
+++ compass.sh
@@ -8,5 +8,7 @@ printf "Compiling Project\n"
 compass compile $scss_path
 
 # Now, watch the project for changes
-printf "Watching Project\n"
-compass watch $scss_path
+compass watch $scss_path > /dev/null 2>/dev/null &
+cid=$! > ./cwatch.pid
+printf "Compass is watcing your project\n"
+printf "PID: $cid"
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Start Vagrant
+vagrant up --provision
+
+# Open the site in the browser
+open http://192.168.33.10
